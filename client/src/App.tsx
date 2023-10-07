cfa26645b4c944b4a63a71255555c962d601e25a
Dipokal HHJ <hhj@devent.kr>
2023-10-07 23:03:08 +0900

    init: clone board

---
+++ client/src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Container } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import RootPage from './pages/Root'
+import NotfoundPage from './pages/Notfound'
+
+import './App.css'
+
+const App = () => {
+    const isDarkmode = useSelector((state: any) => state.app.isDarkmode);
+
+    const darkTheme = createTheme({
+        palette: {
+            mode: isDarkmode === true ? 'dark' : 'light',
+            primary: {
+                main: '#0d6efd',
+            },
+        },
+    });
+
+    return (
+        <div>
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
+
+                <Container color="palette.background.default">
+                    <BrowserRouter>
+                        <Switch>
+                            <Route exact path="/" component={RootPage} />
+
+
+
+                            <Route path='*' component={NotfoundPage} />
+                        </Switch>
+                    </BrowserRouter>
+                </Container>
+            </ThemeProvider>
+        </div>
+    );
+};
+
+export default App;
---
+++ client/src/components/Main.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { Button, Box, Grid } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from "react-router-dom"
+
+
+function Main() {
+
+    return (
+        <Grid container sx={{ marginTop: "3rem" }} spacing={3}>
+
+        <Grid item xs md>
+        </Grid>
+        <Grid item xs={10} md={6}>
+        
+            <Box sx={{ display: 'grid', marginBottom: "2rem", marginTop: "3rem" }}>
+                <Box sx={{ justifyContent: 'center', textAlign: 'center' }}>
+                    <h1>단순 게시판이 아닌데</h1>
+                    <p>환영합니다. 이 게시판은 실험용 게시판 입니다. 글이 언제든지 삭제될 수 있고 서버가 날라갈 우려도 있습니다. DeVent-Frame 프로젝트의 다음 버전 입니다.</p>
+                </Box>
+
+                <Box sx={{ justifyContent: 'center', textAlign: 'center' }}>
+                    
+                </Box>
+
+            </Box>
+        </Grid>
+        <Grid item xs md>
+        </Grid>
+        </Grid>
+
+    );
+}
+
+
+
+export default Main;
