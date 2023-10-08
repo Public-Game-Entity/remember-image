3980e523e8b8a8d33c5f033f5135b100b8dde702
Dipokal HHJ <hhj@devent.kr>
2023-10-08 18:39:27 +0900

    feat: get images

---
+++ script/get.py
@@ -0,0 +1,24 @@
+import requests
+import urllib.request 
+import uuid
+from PIL import Image 
+
+SCRIPT_DIR = '/Users/hhj/Documents/remember-image/script'
+
+pageIndex = 1
+r = requests.get('https://pixabay.com/api/?key=8360759-f7cbcac000e5f5123abe9673c&q=nature&image_type=photo&page='+str(pageIndex))
+getImage = r.json()
+
+
+for index in range(len(getImage['hits'])):
+    url = getImage['hits'][index]['largeImageURL']
+    imageUUID = str(uuid.uuid4())
+    savedUrl = SCRIPT_DIR+"/data/"+imageUUID+".jpg"
+
+    data = requests.get(url).content 
+    f = open(savedUrl,'wb') 
+    f.write(data) 
+    f.close() 
+
+    image = Image.open(savedUrl) 
+    image.save(SCRIPT_DIR+"/result/"+imageUUID+'.webp', 'webp', optimize = True, quality = 70)
